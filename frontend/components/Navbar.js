919bb2293ad58953eaae5b341b431fd056ceacfe
Huzaifa-Faizee <96124697+Huzaifa-Faizee@users.noreply.github.com>
2024-11-20 23:22:05 +0530

    router changes

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,12 +1,18 @@
 export default {
-    template : `
+  template: `
     <div>
         <router-link v-if="!$store.state.loggedIn" to='/'>Home</router-link>
         <router-link v-if="!$store.state.loggedIn" to='/login'>Login</router-link>
         <router-link v-if="!$store.state.loggedIn" to='/register'>Register</router-link>
         <router-link v-if="$store.state.loggedIn && $store.state.role == 'admin'" to='/admin-home'>Home</router-link>
         <router-link v-if="$store.state.loggedIn && $store.state.role == 'admin'" to='/services'>Services</router-link>
-        <button class="btn btn-secondary" v-if="$store.state.loggedIn" @click="$store.commit('logout')">Logout</button>
+        <button class="btn btn-secondary" v-if="$store.state.loggedIn" @click="logout">Logout</button>
     </div>
-    `
-}
+    `,
+  methods: {
+    logout() {
+      this.$store.commit("logout"); 
+      this.$router.push("/");
+    },
+  },
+};
--- frontend/utils/router.js
+++ frontend/utils/router.js
@@ -1,21 +1,44 @@
-const Home = {
-  template: `<h1> this is home </h1>`,
-};
+
 import AdminHome from "../pages/admin/AdminHome.js";
 import Services from "../pages/admin/Services.js";
+import Home from "../pages/Home.js";
 import Login from "../pages/Login.js";
 import Register from "../pages/Register.js";
-
+import store from './store.js'
 const routes = [
   { path: "/", component: Home },
   { path: "/login", component: Login },
   { path: "/register", component: Register },
-  { path: "/admin-home", component: AdminHome },
-  { path: "/services", component: Services },
+  {
+    path: "/admin-home",
+    component: AdminHome,
+    meta: { requiresLogin: true, role: "admin" },
+  },
+  {
+    path: "/services",
+    component: Services,
+    meta: { requiresLogin: true, role: "admin" },
+  },
 ];
 
 const router = new VueRouter({
   routes,
 });
-
+// navigation guards
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresLogin)) {
+    console.log(store.state.loggedIn);
+    
+    if (!store.state.loggedIn) {
+      next({ path: "/login" });
+    } else if (to.meta.role && to.meta.role != store.state.role) {
+      alert("role not authorized");
+      next({ path: "/" });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 export default router;
